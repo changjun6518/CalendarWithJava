c1eb73aa8e757444c44f3ff76d0805da42b1f48f
최창준 <aowert6518@gmail.com>
2021-07-12 12:53:59 +0900

    Step1 clear
    캘린더만들고 커밋하기

---
+++ src/CalendarMain.java
@@ -0,0 +1,18 @@
+public class CalendarMain {
+    public static void main(String[] args) {
+        System.out.println(" 일\t 월 화 수 목 금 토");
+        System.out.println("--------------------");
+        System.out.println(" 1  2  3  4  5  6  7");
+        System.out.println(" 8  9 10 11 12 13 14");
+        System.out.println(" 5 16 17 18 19 20 21");
+        System.out.println("22 23 24 25 26 27 28");
+
+//      일 월 화 수 목 금 토
+//--------------------
+// 1  2  3  4  5  6  7
+// 8  9 10 11 12 13 14
+//15 16 17 18 19 20 21
+//22 23 24 25 26 27 28
+
+    }
+}
